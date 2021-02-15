b0913fb00575e2cbfd38bde66d307ffaa3712e2b
pushkarv <pushkarv@gmail.com>
2021-02-15 11:26:52 -0500

    added insertionsort

---
+++ insertionsort/insertionsort.py
@@ -0,0 +1,27 @@
+#Insertion sort
+#https://en.wikipedia.org/wiki/Insertion_sort
+import numpy as np
+import time
+import math
+import sys
+
+def insertionsort(A):
+    i = 1
+    while i < len(A):
+        j = i
+        while j > 0 and A[j-1] > A[j]:
+            #swap elements
+            A[j], A[j-1] = A[j-1], A[j]
+            j -= 1
+        i += 1
+
+sys.setrecursionlimit(10**6)
+sizes = [10,100,1000,10000,100000,1000000,10000000]
+for k in sizes:
+    A = np.random.randint(1,100,k)
+    start = time.perf_counter()
+    insertionsort(A)
+    end = time.perf_counter()
+    total = end-start
+    print(f"{k} {total}")
+
